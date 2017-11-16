ad89a76d696f8c241027febb78ed5a0b0e68b6b2
Brent Vardy <brent.vardy@challengelogic.net>
2017-11-16 14:16:29 +0000

    complete HW1 Part 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,17 +1,30 @@
 # When done, submit this entire file to the autograder.
 
 # Part 1
-
+# Define a method sum(array) that takes an array of integers as an argument and returns the 
+# sum of its elements. For an empty array it should return zero. 
+# Run associated tests via: $ rspec spec/part1_spec.rb:5
 def sum arr
-  # YOUR CODE HERE
+  arr.inject(0,:+)
 end
 
+# Define a method max_2_sum(array) which takes an array of integers as an argument and returns the 
+# sum of its two largest elements. For an empty array it should return zero. For an array with just 
+# one element, it should return that element. Run associated tests via: $ rspec spec/part1_spec.rb:23
 def max_2_sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  return arr[0] if arr.count == 1
+  arr.sort!.reverse!
+  arr[0] + arr[1]
 end
 
+# Define a method sum_to_n?(array, n) that takes an array of integers and an additional integer, n, as arguments 
+# and returns true if any two elements in the array of integers sum to n. sum_to_n?([], n) should return false for 
+# any value of n, by definition. Run associated tests via: $ rspec spec/part1_spec.rb:42
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  return false if arr.empty? || arr.count == 1
+  sums = arr.combination(2).map { |a,b| a + b }
+  sums.include? n
 end
 
 # Part 2
